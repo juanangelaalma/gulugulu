0f0d0093f8a92feacbc67263e3160a97ab04f288
juanangelaalma <juanlah77@gmail.com>
2022-08-20 21:43:56 +0700

    add dark mode localstorage

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import { Outlet } from "react-router-dom";
 import { Header } from "./containers";
 import { ResultTempProvider } from "./context/ResultTempContext";
 import { SearchProvider } from "./context/SearchContext";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useLocalStorage("dark-mode", false);
+  const [isDarkMode, setIsDarkMode] = useState(darkMode);
 
   useEffect(() => {
     document.title = "Google";
   }, []);
 
+  useEffect(() => {
+    setDarkMode(isDarkMode);
+  }, [isDarkMode]);
+
   return (
     <SearchProvider>
-      <div className={`${darkMode && 'dark'}`}>
+      <div className={`${isDarkMode ? 'dark' : ''}`}>
         <div className="bg-[#FFFFFF] dark:bg-dark-primary min-h-screen max-w-full overflow-x-hidden">
-          <Header />
+          <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
           <ResultTempProvider>
             <Outlet />
           </ResultTempProvider>
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import { BsSun, BsMoon } from 'react-icons/bs'
-import { Brand, Navbar, Searchbar } from '../components';
+import React from "react";
+import { BsSun, BsMoon } from "react-icons/bs";
+import { Brand, Navbar, Searchbar } from "../components";
 
-const Header = () => {
+const Header = ({ isDarkMode, setIsDarkMode }) => {
   return (
     <div>
-      <div className='flex space-x-2 w-full px-5 pt-8 pb-4 items-center'>
+      <div className="flex space-x-2 w-full px-5 pt-8 pb-4 items-center">
         <div className="w-[20%] md:w-[10%]">
           <Brand />
         </div>
-        <div className='w-[70%] md:w-[70%]'>
-        <Searchbar />
+        <div className="w-[70%] md:w-[70%]">
+          <Searchbar />
         </div>
-        <div className='w-[8%] md:w-[20%] flex justify-end'>
-          <BsSun className='text-gray-second dark:text-dark-gray-second' size={30} />
-          {/* <BsMoon size={30} /> */}
+        <div className="w-[8%] md:w-[20%] flex justify-end">
+          {isDarkMode ? (
+            <BsSun
+              className="text-gray-second dark:text-dark-gray-second cursor-pointer"
+              size={30}
+              onClick={() => setIsDarkMode(false)}
+            />
+          ) : (
+            <BsMoon
+              className="text-gray-second dark:text-dark-gray-second cursor-pointer"
+              size={26}
+              onClick={() => setIsDarkMode(true)}
+            />
+          )}
         </div>
       </div>
-      <div className='px-5 md:pl-[12%] border-b border-b-[#EBEBEB] dark:border-b-[#3C4043]'>
+      <div className="px-5 md:pl-[12%] border-b border-b-[#EBEBEB] dark:border-b-[#3C4043]">
         <Navbar />
       </div>
     </div>
